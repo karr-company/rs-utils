d93c25c5b2a5f33217ce2779a20f2df2522bc7b3
Gabriel Duraye <gillianakpeki@hotmail.co.uk>
2025-08-20 19:42:53 +0100

    feat: add ddb parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+use aws_sdk_dynamodb::types::AttributeValue;
 use lambda_http::{Body, Error as LambdaError, Response, http::StatusCode};
 use openssl::error::ErrorStack;
 use openssl::ssl::{SslConnector, SslMethod};
 use postgres::{Client as PostgresClient, Error as PostgresError, Transaction, error::SqlState};
 use postgres_openssl::MakeTlsConnector;
-use serde_json::json;
+use serde::Deserialize;
+use serde_json::{Value, json};
+use std::collections::HashMap;
 
 /// Runs op inside a transaction and retries it as needed.
 /// On non-retryable failures, the transaction is aborted and
@@ -78,6 +81,42 @@ pub fn error_response<E: std::error::Error>(
     Ok(rsp)
 }
 
+pub fn attribute_value_to_json(av: &AttributeValue) -> Value {
+    if let Some(s) = av.as_s().ok() {
+        Value::String(s.to_string())
+    } else if let Some(n) = av.as_n().ok() {
+        // Try to parse number as f64 or i64 depending on your use case
+        if let Ok(int_val) = n.parse::<i64>() {
+            Value::Number(int_val.into())
+        } else if let Ok(float_val) = n.parse::<f64>() {
+            Value::Number(serde_json::Number::from_f64(float_val).unwrap())
+        } else {
+            Value::String(n.to_string())
+        }
+    } else if let Some(m) = av.as_m().ok() {
+        let map_json: serde_json::Map<String, Value> = m
+            .iter()
+            .map(|(k, v)| (k.clone(), attribute_value_to_json(v)))
+            .collect();
+        Value::Object(map_json)
+    } else if let Some(l) = av.as_l().ok() {
+        let list_json: Vec<Value> = l.iter().map(attribute_value_to_json).collect();
+        Value::Array(list_json)
+    } else if av.as_bool().is_ok() {
+        Value::Bool(av.as_bool().unwrap().clone())
+    } else if av.is_null() {
+        Value::Null
+    } else {
+        Value::Null // fallback for other types like Binary etc.
+    }
+}
+
+pub fn item_to_json_map(item: &HashMap<String, AttributeValue>) -> serde_json::Map<String, Value> {
+    item.iter()
+        .map(|(k, v)| (k.clone(), attribute_value_to_json(v)))
+        .collect()
+}
+
 #[derive(Debug, thiserror::Error)]
 pub enum CertError {
     #[error("OpenSSL error")]
@@ -85,4 +124,10 @@ pub enum CertError {
 
     #[error("Reqwest error")]
     ReqwestError(#[from] reqwest::Error),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DynamicItem {
+    #[serde(flatten)]
+    fields: HashMap<String, Value>,
 }
